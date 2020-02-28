87c42c0d9fdcf360100be1c912b8cfd17991cf28
Ignacio Giadach <igiadach@gmail.com>
2020-02-28 14:54:31 +1100

    added Audio and Comment models

---
+++ db/migrate/20200228035147_create_audios.rb
@@ -0,0 +1,10 @@
+class CreateAudios < ActiveRecord::Migration[5.2]
+  def change
+    create_table :audios do |t|
+      t.string :url
+      t.references :track, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_28_032522) do
+ActiveRecord::Schema.define(version: 2020_02_28_035147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "audios", force: :cascade do |t|
+    t.string "url"
+    t.bigint "track_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["track_id"], name: "index_audios_on_track_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.bigint "track_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["track_id"], name: "index_comments_on_track_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
@@ -46,6 +64,9 @@ ActiveRecord::Schema.define(version: 2020_02_28_032522) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "audios", "tracks"
+  add_foreign_key "comments", "tracks"
+  add_foreign_key "comments", "users"
   add_foreign_key "tracks", "genres"
   add_foreign_key "tracks", "users"
 end
