cb5dcf7dcfc38ef4a48632e99bc9fbf30b2525af
Ignacio Giadach <igiadach@gmail.com>
2020-03-03 13:13:04 +1100

    seeding working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# for i in 1..2
+#     User.create(
+#         username: "username#{i}",
+#         email: "#{i}@1.com",
+#         password: "password"
+#     )
+#     puts "created #{i} users"
+# end
+
 for i in 1..10
     Track.create(
         title: Faker::Music.band,
         album: Faker::Music.album,
         description: Faker::Quote.famous_last_words,
+        user_id: Faker::Number.between(from: 1, to: 2),
+        donate: Faker::Number.between(from: 1, to: 20),
+        genre_id: Faker::Number.between(from: 1, to: 2)
+
     )
 
     puts "Created #{i} tracks"
